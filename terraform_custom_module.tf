7e5c6ee4dc7bc632ac8cd5eb06dc4336af33c931
Ankesh Kedarnath <ankesh.kedarnath@hach.com>
2020-09-21 00:06:32 +0530

    first commit

---
+++ terraform_custom_module.tf
@@ -0,0 +1,46 @@
+provider "azurerm" {
+  version = "=2.20.0"
+  features {}
+}
+
+module "az_resource_group" {
+  source   = "../terraform-azure-resource-group"
+  name     = "ankesh-workspace"
+  location = "westeurope"
+  team_tag = "DevOps"
+  creator  = "ankesh"
+}
+
+module "az_virtual_network"{
+  source = "../terraform-azure-virtual-network"
+  name = "ankesh-vnet"
+  resource_group_name = module.az_resource_group.az_rg_name
+  address_space = "10.0.2.0/24"
+  env = "dev"
+  team_tag = "DevOps"
+  creator  = "ankesh"
+}
+
+output "azure_resource_group_id" {
+  value = module.az_resource_group.az_rg_id
+}
+
+output "azure_resource_group_name" {
+  value = module.az_resource_group.az_rg_name
+}
+
+output "azure_vnet_id" {
+  value = module.az_virtual_network.vnet_id
+}
+
+output "azure_vnet_name" {
+  value = module.az_virtual_network.vnet_name
+}
+
+output "azure_vnet_location" {
+  value = module.az_virtual_network.vnet_location
+}
+
+output "azure_vnet_address_space" {
+  value = module.az_virtual_network.vnet_address_space
+}
