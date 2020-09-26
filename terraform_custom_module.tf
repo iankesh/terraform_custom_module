12de087bef8d7280d91cbbc501ab7a728ad5b67e
Ankesh Kedarnath <ankesh.kedarnath@hach.com>
2020-09-27 03:03:27 +0530

    adding terraform module for azure security group creation

--- terraform_custom_module.tf
+++ terraform_custom_module.tf
@@ -1,6 +1,8 @@
 # Module List:
-# iankesh/resource-group/azure
-# ankesh/virtual-network/azure
+# | iankesh/resource-group/azure
+# | iankesh/virtual-network/azure
+# | iankesh/subnet/azure
+# | iankesh/security-group/azure
 
 module "az_resource_group" {
   source   = "../terraform-azure-resource-group"
@@ -27,4 +29,22 @@ module "az_subnet" {
   resource_group_name = module.az_resource_group.az_rg_name
   vnet_name           = module.az_virtual_network.az_vnet_name
   address_prefix      = "10.0.2.0/26"
+}
+
+module "az_security_group" {
+  source                              = "../terraform-azure-security-group"
+  name                                = "ankesh-security-group"
+  resource_group_name                 = module.az_resource_group.az_rg_name
+  security_rule_name                  = "ankesh-security-rule"
+  security_priority                   = "101"
+  security_direction                  = "Inbound"
+  security_access                     = "Allow"
+  security_protocol                   = "Tcp"
+  security_source_port                = "*"
+  security_destination_port           = ["80" , "22", "443"]
+  security_source_address_prefix      = "*"
+  security_destination_address_prefix = "*"
+  env                                 = "dev"
+  team_tag                            = "DevOps"
+  creator                             = "ankesh"
 }
