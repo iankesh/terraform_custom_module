5d146b057e347fdbd8c4077f840afb0edef14770
Ankesh Kedarnath <ankesh.kedarnath@hach.com>
2020-09-26 21:58:29 +0530

    adding terraform module for azure subnet creation

---
+++ output.tf
@@ -0,0 +1,23 @@
+output "azure_resource_group_name" {
+  value = module.az_resource_group.az_rg_name
+}
+
+output "azure_vnet_name" {
+  value = module.az_virtual_network.az_vnet_name
+}
+
+output "azure_vnet_location" {
+  value = module.az_virtual_network.az_vnet_location
+}
+
+output "azure_vnet_address_space" {
+  value = module.az_virtual_network.az_vnet_address_space
+}
+
+output "azure_subnet_name" {
+  value = module.az_subnet.az_subnet_name
+}
+
+output "azure_subnet_address_prefix" {
+  value = module.az_subnet.az_subnet_address_prefix
+}
--- terraform_custom_module.tf
+++ terraform_custom_module.tf
@@ -2,11 +2,6 @@
 # iankesh/resource-group/azure
 # ankesh/virtual-network/azure
 
-provider "azurerm" {
-  version = "=2.20.0"
-  features {}
-}
-
 module "az_resource_group" {
   source   = "../terraform-azure-resource-group"
   name     = "ankesh-workspace"
@@ -26,26 +21,10 @@ module "az_virtual_network" {
   creator             = "ankesh"
 }
 
-output "azure_resource_group_id" {
-  value = module.az_resource_group.az_rg_id
-}
-
-output "azure_resource_group_name" {
-  value = module.az_resource_group.az_rg_name
-}
-
-output "azure_vnet_id" {
-  value = module.az_virtual_network.vnet_id
-}
-
-output "azure_vnet_name" {
-  value = module.az_virtual_network.vnet_name
-}
-
-output "azure_vnet_location" {
-  value = module.az_virtual_network.vnet_location
-}
-
-output "azure_vnet_address_space" {
-  value = module.az_virtual_network.vnet_address_space
+module "az_subnet" {
+  source              = "../terraform-azure-subnet"
+  name                = "ankesh-subnet"
+  resource_group_name = module.az_resource_group.az_rg_name
+  vnet_name           = module.az_virtual_network.az_vnet_name
+  address_prefix      = "10.0.2.0/26"
 }
